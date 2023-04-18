4707d23d750102d7462b592a75bcb5e1588c353b
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-04-18 17:41:29 +0000

    async

--- routes/api.php
+++ routes/api.php
@@ -28,7 +28,6 @@ Route::group(['prefix' => 'V1'], function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
 
-
     Route::group(['middleware' => ['auth:sanctum']], function () {
         Route::post('logout', [AuthController::class, 'logout']);
 
